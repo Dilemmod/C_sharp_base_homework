35c0188074f21e732cdc715a88712a4025ba4179
Dileon <yevtayev@gmail.com>
2019-09-18 22:15:06 +0300

    Lesson_02

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your check amount");
+            int m = Convert.ToInt32(Console.ReadLine());
+            byte z;
+            if (m >= 500) z = 5;
+            else if (m >= 400) z = 3;
+            else if (m >= 300) z = 2;
+            else z = 0;
+            Console.WriteLine($"Your discound is {z}%, total to pay {m-(m*z/100)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_2/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter how  much money do you have");
+            int m = Convert.ToInt32(Console.ReadLine());
+            if (m >= 80) Console.WriteLine($"You can buy 1 Leffe, the rest {m - 80}");
+            else if (m >= 70) Console.WriteLine($"You can buy 1 Hoegarsen, the rest {m - 70}");
+            else if (m >= 60) Console.WriteLine($"You can buy 1 Stella, the rest {m - 60}");
+            else if (m >= 7) Console.WriteLine($"You can buy 1 Obolon, the rest {m - 7}");
+            else Console.WriteLine($"You can't buy any beer"); 
+            Console.ReadKey();
+        }
+    }
+}
