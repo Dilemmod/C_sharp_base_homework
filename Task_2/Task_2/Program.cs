e61ad2800de1e8ff85e9f2047aac550d506e2412
Dileon <yevtayev@gmail.com>
2019-09-18 23:32:30 +0300

    Lesson_02 Without more WriteLine

--- Task_2/Task_2/Program.cs
+++ Task_2/Task_2/Program.cs
@@ -12,11 +12,30 @@ namespace Task_2
         {
             Console.WriteLine("Enter how  much money do you have");
             int m = Convert.ToInt32(Console.ReadLine());
-            if (m >= 80) Console.WriteLine($"You can buy 1 Leffe, the rest {m - 80}");
-            else if (m >= 70) Console.WriteLine($"You can buy 1 Hoegarsen, the rest {m - 70}");
-            else if (m >= 60) Console.WriteLine($"You can buy 1 Stella, the rest {m - 60}");
-            else if (m >= 7) Console.WriteLine($"You can buy 1 Obolon, the rest {m - 7}");
-            else Console.WriteLine($"You can't buy any beer"); 
+            string b = "";
+            if (m >= 80)
+            {
+                b = "leffe";
+                m -= 80;
+            }
+            else if (m >= 70)
+            {
+                b = "Hoegarsen";
+                m -= 70;
+            }
+            else if (m >= 60)
+            {
+                b = "Stella";
+                m -= 60;
+            }
+            else if (m >= 7)
+            {
+                b = "Obolon";
+                m -= 7;
+            }
+            else Console.WriteLine($"You can't buy any beer");
+
+            Console.WriteLine($"You can buy 1 {b}, the rest {m}");
             Console.ReadKey();
         }
     }
