fb60f625218e642520ec3efb4a6200f481795877
Dileon <yevtayev@gmail.com>
2019-10-07 11:43:41 +0300

    L_7 Task1, Task_2

---
+++ Task_1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Product
+    {
+        protected string name, color;
+        protected int price;
+        protected double weigth;
+        public Product(string name, int price, string color,double weigth)
+        {
+            this.name = name;
+            this.price = price;
+            this.weigth = weigth;
+            this.color = color;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public int getPrice()
+        {
+            return price;
+        }
+        public string getColor()
+        {
+            return color;
+        }
+        public double getWeigth()
+        {
+            return weigth;
+        }
+    }
+    class Cart
+    {
+        Product[] products = new Product[10];
+        public void addProduct(Product product)
+        {
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i] == null)
+                {
+                    products[i] = product;
+                    break;
+                }
+            }
+        }
+        public void removeProduct(string name)
+        {
+            for(int i = 0; i < products.Length; i++)
+            {
+                if (products[i].getName() == name)
+                {
+                    products[i] = null;
+                    break;
+                }
+            }
+        }
+        public void showCart()
+        {
+            Console.WriteLine("CART:");
+            int total = 0;
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i] != null)
+                {
+                    Console.WriteLine("\t" + products[i].getName() + " : " + products[i].getPrice() + "\tcolor:" + products[i].getColor() + "\tweigth:" + products[i].getWeigth());
+                    total += products[i].getPrice();
+                }
+            }
+            Console.WriteLine("CART TOTAL: " + total);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product("Coca-Cola", 15, "Black",1.5);
+            Product p2 = new Product("Pepsi-Cola", 16,"Grey",1.5);
+            Product p3 = new Product("Milk", 26,"White", 1);
+            Product p4 = new Product("Bread", 12,"Brown",0.5);
+            Product p5 = new Product("Butter", 35,"Yellow",0.2);
+            Cart cart = new Cart();
+            cart.addProduct(p1);
+            cart.addProduct(p3);
+            cart.addProduct(p4);
+            cart.addProduct(p5);
+            cart.showCart();
+            Console.WriteLine("Enter the name of the product you want to remove ");
+            cart.removeProduct(Console.ReadLine());
+            cart.showCart();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Figure
+    {
+        protected double radius, width, height, side1, side2, side3;
+        Random rand = new Random();
+        string[] colors = { "Red", "Blue", "Green", "Black", "White" };
+        public string getColor()
+        {
+            return colors[rand.Next(0, colors.Length)];
+        }
+        public void getInfo(string style, double perimeter, double area)
+        {
+            Console.Write("Name: " + style + "; Color: " + getColor() + "; Perimeter: " + perimeter + "; Area: " + area);
+        }     
+    }
+    class Circle : Figure
+    {
+        const double PI = 3.141592653589793238463;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            getInfo("Cyrcle", 2 * PI * radius, PI * (radius *= radius));
+        }
+    }
+    class Rectangle : Figure
+    {
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+            getInfo((width == height ? "Square" : "Rectangle"), 2 * (width + height), width * height / 2);
+        }
+    }
+    class Triangle : Figure
+    {
+        public Triangle(double side1,double side2, double side3)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+            double p = 0.5 * (side1 + side2 + side3);
+            getInfo((side1 != side2 && side1 != side3 && side2 != side3 ? "Versatile" : (side1 == side2 && side1 == side3 && side2 == side3 ? "Equilateral" : "Isosceles")), side1 + side2 + side3, Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3)));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string number;
+            Console.WriteLine("Enter your figure: ");
+            Console.WriteLine("\tFor circle 1, for rectangle 2, for triangle 3");
+            number = Console.ReadLine();
+            switch(number)
+            {
+                case "1":
+                    Console.Write("Enter radius = ");
+                    Figure circle = new Circle(Convert.ToDouble(Console.ReadLine()));
+                    break;
+                case "2":
+                    Console.Write("Enter width = ");
+                    double w = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter height = ");
+                    Figure rectangle = new Rectangle(w, Convert.ToDouble(Console.ReadLine()));
+                    break;
+                case "3":
+                    Console.Write("Enter side 1 = ");
+                    double side1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter side 2 = ");
+                    double side2 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter side 3 = ");
+                    Figure triangle = new Triangle(side1, side2, Convert.ToDouble(Console.ReadLine()));
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
